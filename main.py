ca5f89f37696100ba8629846158e0b3460f2f4cc
Hasit211 <73527213+Hasit211@users.noreply.github.com>
2021-04-21 10:41:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+f=open('hasit.txt')
+filelines = f.readlines()
+for line in filelines:
+    print(line)
+ #introSring='my,name is hasit ,and i am 15 years, old'
+#words=introSring.split(',')
+#print(words)
+#def greet(name):
+ #   print('hello '+name+ '. how are you?')
+#greet('hasit')
+
+#def countWordsFromFile():
+ #   fileName = input('hasit.txt')
+  #  numberOfWords = 0
+   # file = open(fileName,'r')
+    #for line in file:
+     #   word = line.split()
+      #  numberOfWords = numberOfWords+len(word)
+  #  print(numberOfWords)
+  #countWordsFromFile()#
+
+#def swapFileData():
+ #   with open(hasit2.txt,'r') as a:
+  #      data_a = a.read()
+   # with open(hasit.txt,'r') as b:
+    #    data_b = b.read()
+    #with open(hasit2.txt,'w') as a:
+     #   a.write(data_b)
+   # with open(hasit.txt,'w') as b:
+    #    b.write(data_a)
+#swapFileData() 
